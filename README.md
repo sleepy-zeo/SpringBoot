a6a58588808d38d883f40cf1e6844d35c6fa3f2e
wuhongwei <hongwei.wu@grandsun.com>
2020-11-02 16:42:21 +0800

    Init repo SpringBoot

---
+++ README.md
@@ -0,0 +1,5 @@
+# SpringBoot
+
+## 什么是Spring Boot
+
+本质上是一个通过pom.xml构建的一个项目。Spring Boot简化了使用Spring的各种繁琐配置，提供了各种启动器，使得开发者能快速上手
---
+++ src/main/java/com/sleepy/zeo/springboot/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.sleepy.zeo.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("")
+    @ResponseBody
+    public String index() {
+        return "This is index";
+    }
+}
